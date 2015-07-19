b0f5d20c63524911156295138f5ea9d7e8f5209a
Leonel Samayoa <leonel.e.samayoa@gmail.com>
2015-07-19 17:11:48 -0600

    ISSUE-2: Added some memo creation policies, also added an audit controller advice for general resource not found messages.

--- src/main/java/com/gobliip/chronos/server/service/MomentsService.java
+++ src/main/java/com/gobliip/chronos/server/service/MomentsService.java
@@ -4,6 +4,9 @@ import com.gobliip.chronos.server.audit.ResourceAudit;
 import com.gobliip.chronos.server.entities.Moment;
 import com.gobliip.chronos.server.entities.MomentsRepository;
 import com.gobliip.chronos.server.entities.Tracking;
+import com.gobliip.chronos.server.service.exception.InvalidTrackingStateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,28 @@ import java.time.Instant;
 @Service
 public class MomentsService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MomentsService.class);
+
+    public enum TrackingAction {
+        CREATE_MEMO("create", "moment", "memo");
+        private final String action;
+        private final String[] params;
+        TrackingAction(String action, String... params){
+            this.action = action;
+            this.params = params;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder(action);
+            for (String param : params){
+                builder.append(' ');
+                builder.append(param);
+            }
+            return builder.toString();
+        }
+    }
+
     @Autowired
     private MomentsRepository momentsRepository;
 
@@ -27,26 +52,40 @@ public class MomentsService {
     @ResourceAudit
     @Secured("ROLE_USER")
     @Transactional(readOnly = false)
-    public Moment createMoment(String ownerId, Long trackingId, Moment.MomentType type, byte[] attachment, String memo) {
+    public Moment createMoment(String ownerId, Long trackingId, Moment.MomentType type, byte[] attachment, String memo)
+            throws InvalidTrackingStateException {
         Assert.hasText(ownerId);
         Assert.notNull(trackingId);
         Assert.state(trackingId > 0);
         Assert.notNull(type);
-        Assert.state(verifyTypeCreationSupport(type));
+        Assert.state(verifyMomentTypeCreationSupport(type));
 
-        Tracking tracking = trackingsService.findTracking(ownerId, trackingId);
+        final Tracking tracking = trackingsService.findTracking(ownerId, trackingId);
 
-        Moment moment = new Moment();
+        final Moment moment = new Moment();
         moment.setAttachment(attachment);
         moment.setTracking(tracking);
         moment.setMemo(memo);
         moment.setMomentInstant(Instant.now());
         moment.setType(type);
 
+        if (!validateTrackingAction(tracking, TrackingAction.CREATE_MEMO)) {
+            LOGGER.error("Tracking currently not running, imposibble to create new moment: {}", moment);
+            throw new InvalidTrackingStateException(tracking, TrackingAction.CREATE_MEMO);
+        }
+
         return momentsRepository.save(moment);
     }
 
-    public boolean verifyTypeCreationSupport(Moment.MomentType type) {
+    public boolean validateTrackingAction(Tracking tracking, TrackingAction action){
+        switch (action){
+            case CREATE_MEMO:
+                return Tracking.TrackingStatus.RUNNING.equals(tracking.getStatus());
+        }
+        return false;
+    }
+
+    public boolean verifyMomentTypeCreationSupport(Moment.MomentType type) {
         switch (type) {
             // Only allow memo creation through this service
             case MEMO:

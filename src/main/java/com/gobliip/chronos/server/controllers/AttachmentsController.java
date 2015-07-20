a22a9f8f1af0280f2e3d6795e1633fa524793317
Leonel Samayoa <leonel.e.samayoa@gmail.com>
2015-07-19 18:50:28 -0600

    ISSUE-2: Added a way to obtain the contents of attachments

---
+++ src/main/java/com/gobliip/chronos/server/controllers/AttachmentsController.java
@@ -0,0 +1,30 @@
+package com.gobliip.chronos.server.controllers;
+
+import com.gobliip.chronos.server.service.AttachmentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * Created by lsamayoa on 19/07/15.
+ */
+@RestController
+@RequestMapping("/attachments")
+public class AttachmentsController {
+
+    @Autowired
+    private AttachmentsService attachmentsService;
+
+    @RequestMapping("/{attachmentId}/raw")
+    public byte[] getRaw(
+            @AuthenticationPrincipal Principal principal,
+            @PathVariable("attachmentId") Long attachmentId
+    ) {
+        return attachmentsService.getRaw(principal.getName(), attachmentId);
+    }
+    
+}

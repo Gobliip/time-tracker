593d2537d81eeccada1788db3afc5f7545e7ccba
Leonel Samayoa <leonel.e.samayoa@gmail.com>
2015-07-20 20:31:49 -0600

    ISSUE-10: correcte formating of some files

--- src/main/java/com/gobliip/chronos/domain/exception/UnpausableTrackingException.java
+++ src/main/java/com/gobliip/chronos/domain/exception/UnpausableTrackingException.java
@@ -4,36 +4,36 @@ import com.gobliip.chronos.server.entities.Tracking.TrackingStatus;
 
 public class UnpausableTrackingException extends Exception {
 
-	private static final long serialVersionUID = 8341936779233448784L;
-	
-	
-	private Long trackingId;
-	private TrackingStatus currentStatus;
-	
-	public UnpausableTrackingException(Long trackingId, TrackingStatus currentStatus) {
-		this.trackingId = trackingId;
-		this.currentStatus = currentStatus;
-	}
-	
-	@Override
-	public String getMessage() {
-		return "Tracking with id: " + trackingId +" with status: "+currentStatus+" is an unpausable.";	
-	}
-	
-	public Long getTrackingId() {
-		return trackingId;
-	}
+    private static final long serialVersionUID = 8341936779233448784L;
 
-	public void setTrackingId(Long trackingId) {
-		this.trackingId = trackingId;
-	}
 
-	public TrackingStatus getCurrentStatus() {
-		return currentStatus;
-	}
+    private Long trackingId;
+    private TrackingStatus currentStatus;
 
-	public void setCurrentStatus(TrackingStatus currentStatus) {
-		this.currentStatus = currentStatus;
-	}
+    public UnpausableTrackingException(Long trackingId, TrackingStatus currentStatus) {
+        this.trackingId = trackingId;
+        this.currentStatus = currentStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Tracking with id: " + trackingId + " with status: " + currentStatus + " is an unpausable.";
+    }
+
+    public Long getTrackingId() {
+        return trackingId;
+    }
+
+    public void setTrackingId(Long trackingId) {
+        this.trackingId = trackingId;
+    }
+
+    public TrackingStatus getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public void setCurrentStatus(TrackingStatus currentStatus) {
+        this.currentStatus = currentStatus;
+    }
 
 }
--- src/main/java/com/gobliip/chronos/server/audit/ResourceAuditAspect.java
+++ src/main/java/com/gobliip/chronos/server/audit/ResourceAuditAspect.java
@@ -22,60 +22,60 @@ import com.gobliip.chronos.domain.exception.ResourceNotOwnedException;
 @Component
 public class ResourceAuditAspect implements ApplicationEventPublisherAware {
 
-	@Autowired
-	private ApplicationEventPublisher applicationEventPublisher;
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
 
-	@Override
-	public void setApplicationEventPublisher(
-			ApplicationEventPublisher applicationEventPublisher) {
-		this.applicationEventPublisher = applicationEventPublisher;
-	}
+    @Override
+    public void setApplicationEventPublisher(
+            ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
 
-	@AfterThrowing(pointcut = "@annotation(resource)", throwing = "ex")
-	public void auditResoruceNotOwned(ResourceNotOwnedException ex, ResourceAudit resource) {
-		String principal = ex.getPrincipal() != null ? ex.getPrincipal()
-				: (String) SecurityContextHolder.getContext()
-						.getAuthentication().getPrincipal();
-		// @formatter:off
-		AuditEvent illegalAccessEvent =  new AuditEvent(
-				principal, "RESOURCE_ACCESS_ILLEGAL", 
-				"RESOURCE_ID=" + ex.getResourceId().toString(),
-				"RESOURCE_TYPE=" + ex.getResourceType());
-		//@formatter:on
+    @AfterThrowing(pointcut = "@annotation(resource)", throwing = "ex")
+    public void auditResoruceNotOwned(ResourceNotOwnedException ex, ResourceAudit resource) {
+        String principal = ex.getPrincipal() != null ? ex.getPrincipal()
+                : (String) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        // @formatter:off
+        AuditEvent illegalAccessEvent = new AuditEvent(
+                principal, "RESOURCE_ACCESS_ILLEGAL",
+                "RESOURCE_ID=" + ex.getResourceId().toString(),
+                "RESOURCE_TYPE=" + ex.getResourceType());
+        //@formatter:on
 
-		applicationEventPublisher.publishEvent(new AuditApplicationEvent(
-				illegalAccessEvent));
-	}
+        applicationEventPublisher.publishEvent(new AuditApplicationEvent(
+                illegalAccessEvent));
+    }
 
-	@AfterThrowing(pointcut = "@annotation(resource)", throwing = "ex")
-	public void auditResourceNotFound(ResourceNotFoundException ex, ResourceAudit resource) {
-		String principal = (String) SecurityContextHolder.getContext()
-				.getAuthentication().getPrincipal();
-		// @formatter:off
-		AuditEvent illegalAccessEvent =  new AuditEvent(
-				principal, "RESOURCE_ACCESS_NOT_FOUND", 
-				"RESOURCE_ID=" + ex.getResourceId(),
-				"RESOURCE_TYPE=" + ex.getResourceType());
-		//@formatter:on
+    @AfterThrowing(pointcut = "@annotation(resource)", throwing = "ex")
+    public void auditResourceNotFound(ResourceNotFoundException ex, ResourceAudit resource) {
+        String principal = (String) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        // @formatter:off
+        AuditEvent illegalAccessEvent = new AuditEvent(
+                principal, "RESOURCE_ACCESS_NOT_FOUND",
+                "RESOURCE_ID=" + ex.getResourceId(),
+                "RESOURCE_TYPE=" + ex.getResourceType());
+        //@formatter:on
 
-		applicationEventPublisher.publishEvent(new AuditApplicationEvent(
-				illegalAccessEvent));
-	}
+        applicationEventPublisher.publishEvent(new AuditApplicationEvent(
+                illegalAccessEvent));
+    }
 
-	@After("@annotation(resource) && args(resourceId,..)")
-	public void auditResourceAccess(JoinPoint jp, ResourceAudit resource,
-			Serializable resourceId) throws Throwable {
-		String principal = (String) SecurityContextHolder.getContext()
-				.getAuthentication().getPrincipal();
-		// @formatter:off
-		AuditEvent resourceAccessEvent = new AuditEvent(
-				principal, "RESOURCE_ACCESS", 
-				"RESOURCE_TYPE=" + ((MethodSignature)jp.getSignature()).getReturnType().getName(),
-				"RESOURCE_ID=" + resourceId
-				);
-		// @formatter:on
-		applicationEventPublisher.publishEvent(new AuditApplicationEvent(
-				resourceAccessEvent));
-	}
+    @After("@annotation(resource) && args(resourceId,..)")
+    public void auditResourceAccess(JoinPoint jp, ResourceAudit resource,
+                                    Serializable resourceId) throws Throwable {
+        String principal = (String) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        // @formatter:off
+        AuditEvent resourceAccessEvent = new AuditEvent(
+                principal, "RESOURCE_ACCESS",
+                "RESOURCE_TYPE=" + ((MethodSignature) jp.getSignature()).getReturnType().getName(),
+                "RESOURCE_ID=" + resourceId
+        );
+        // @formatter:on
+        applicationEventPublisher.publishEvent(new AuditApplicationEvent(
+                resourceAccessEvent));
+    }
 
 }
